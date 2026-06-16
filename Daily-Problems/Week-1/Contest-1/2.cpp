aa9090121229bfef67a15a970fab1b61981bb7d5
ankit <rathaurankit501@gmail.com>
2026-06-16 09:55:41 +0530

    Create 2.cpp

---
+++ Daily-Problems/Week-1/Contest-1/2.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long X, Y;
+    cin >> X >> Y;
+    long long prod= X*Y;
+
+    if (prod>=0) {
+        if (prod % 2 == 0)
+            cout << "ALPHA"<<endl;
+        else
+            cout << "BETA"<<endl;
+       }
+    else {
+        long long sum = abs(X) + abs(Y);
+
+        if (sum % 3 == 0)
+            cout << "GAMMA"<<endl;
+        else
+            cout << "DELTA"<<endl;
+    }
+
+    return 0;
+}
